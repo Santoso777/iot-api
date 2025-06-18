baadb4d72d10e718be5c9e3229818809a9386ae8
santoso777 <edy.sant3321@gmail.com>
2025-06-18 19:24:52 +0700

    commit ke 2

--- index.js
+++ index.js
@@ -102,8 +102,9 @@ app.get('/grafik', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'tabel.html'));
   });
   
-  
+  console.log("coba commit 2")
 // Jalankan server
 app.listen(PORT, () => {
   console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
+
